bc99ac965b741516ca759b2849bb7914c9338f3d
iamKushGupta <74309241+iamKushGupta@users.noreply.github.com>
2023-04-27 07:35:45 +0530

    more comments

--- bird-in-space.py
+++ bird-in-space.py
@@ -5,9 +5,16 @@ import random
 # Initializes all imported pygame modules
 pygame.init()
 
-# Creates a clock object that will help us control the frame rate of our game
+# Creates a clock object that will help us control
+# the frame rate of our game
 clock = pygame.time.Clock()
 fps = 60  # Sets the frame rate to 60 FPS
+"""
+If frame rate not set, then the game will try to run all the codes as soon as possible.
+As in, the ground would end before the could end.
+FPS sets the game's speed for all speeds. 
+"""
+
 
 # Set screen display size
 screen_width = 864
@@ -22,10 +29,10 @@ font = pygame.font.SysFont('Bauhaus 93', 60)
 white = (255, 255, 255)
 
 # define game variables
-ground_scroll = 0
-scroll_speed = 4
-flying = False
-game_over = False  # Default
+ground_scroll = 0  # ground scroll speed
+scroll_speed = 4  # the scrolling speed
+flying = False # setting the game start event via this variable
+game_over = False  
 pipe_gap = 200  # Distance between 2 obstacles
 pipe_frequency = 1500  # milliseconds
 last_pipe = pygame.time.get_ticks() - pipe_frequency
@@ -54,38 +61,42 @@ def reset_game():
     score = 0
     return score
 
+# Bird game using Pygame's Sprite classes
+
 
 class Bird(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
+        # update and draw already pre-build into Sprite classes
         pygame.sprite.Sprite.__init__(self)
-        self.images = []
+        self.images = []  # the image that the sprite is going to be assigned
         self.index = 0
-        self.counter = 0
+        self.counter = 0  # controls the animatoin speed (bird's flutter)
+        # the loop appends the images one after other, to create an animation in the bird
         for num in range(1, 4):
             img = pygame.image.load(f"img/bird{num}.png")
-            self.images.append(img)
+            self.images.append(img)  # appends images in the list made before
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
+        self.rect.center = [x, y]  # decides the location of the bird
         self.vel = 0
         self.clicked = False
-
+    # 
     def update(self):
-
         if flying == True:
-            # apply gravity
+            # Gravity implemetation
             self.vel += 0.5
-            if self.vel > 8:
-                self.vel = 8
-            if self.rect.bottom < 768:
+            if self.vel > 8: # limits velocity
+                self.vel = 8 # caps vel to 8
+            if self.rect.bottom < 768: # ground coordinate=768, limits
                 self.rect.y += int(self.vel)
 
         if game_over == False:
-            # jump
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+            # Fly the bird (JUMP)
+            #self.clicked ensures that we can't hold onto the button, and keep going up.
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:  
                 self.clicked = True
-                self.vel = -10
+                self.vel = -10 # jumps by 2 (opposite to gravity)
             if pygame.mouse.get_pressed()[0] == 0:
                 self.clicked = False
 
@@ -94,15 +105,16 @@ class Bird(pygame.sprite.Sprite):
             self.counter += 1
 
             if self.counter > flap_cooldown:
-                self.counter = 0
-                self.index += 1
-                if self.index >= len(self.images):
-                    self.index = 0
+                self.counter = 0 # reset to zero
+                self.index += 1 # move to the next bird image to make flutter
+                if self.index >= len(self.images): # if it comes to the end of list (3), we reset 
+                    self.index = 0 # resets to index=0
                 self.image = self.images[self.index]
 
-            # rotate the bird
+            # Rotate the bird
             self.image = pygame.transform.rotate(
-                self.images[self.index], self.vel * -2)
+                self.images[self.index], self.vel * -2) # rotation wrt to velocity
+            # * (-2) points the bird up once we click.
         else:
             # point the bird at the ground
             self.image = pygame.transform.rotate(self.images[self.index], -90)
@@ -161,21 +173,21 @@ bird_group.add(flappy)
 # create restart button instance
 button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
-
+# make the game run. continuous loop until the break condition happens
 run = True
 while run:
-
     clock.tick(fps)
 
     # draw background
-    screen.blit(bg, (0, 0))
+    screen.blit(bg, (0, 0))  # sets the background.
 
     pipe_group.draw(screen)
     bird_group.draw(screen)
     bird_group.update()
 
     # draw and scroll the ground
-    screen.blit(ground_img, (ground_scroll, 768))
+    # ground_scroll sets the scroll speed for the ground, to the left
+    screen.blit(ground_img, (ground_scroll, 768)) # 768 = y-coordinate of ground
 
     # check the score
     if len(pipe_group) > 0:
